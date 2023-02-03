eb846640d5afc7f3af33c51a0d984697e188d28a
Caroline Lins <carolineglins@gmail.com>
2023-02-03 15:03:03 -0300

    Editando restaurantes

--- src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -1,6 +1,7 @@
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
 import axios from "axios"
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 import IRestaurante from "../../../interfaces/IRestaurante"
 
 const AdministracaoRestaurantes = () => {
@@ -21,11 +22,15 @@ const AdministracaoRestaurantes = () => {
                         <TableCell>
                             Nome
                         </TableCell>
+                        <TableCell>
+                            Editar
+                        </TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {restaurantes.map(restaurante => <TableRow key={restaurante.id}>
                         <TableCell>{restaurante.nome}</TableCell>
+                        <TableCell> [ <Link to={`/admin/restaurantes/${restaurante.id}`} >editar</Link> ]</TableCell>
                     </TableRow>)}
                 </TableBody>
             </Table>
