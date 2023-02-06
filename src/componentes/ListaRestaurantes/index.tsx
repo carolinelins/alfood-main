2eb60b0493c0360d923a58e367dd1ffcf4134571
Caroline Lins <carolineglins@gmail.com>
2023-02-06 09:22:17 -0300

    Desafio: obtendo os pratos via API

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,22 +1,38 @@
-import axios from 'axios'
+import { TextField } from '@mui/material'
+import axios, { AxiosRequestConfig } from 'axios'
 import { useEffect, useState } from 'react'
 import { IPaginacao } from '../../interfaces/IPaginacao'
 import IRestaurante from '../../interfaces/IRestaurante'
 import style from './ListaRestaurantes.module.scss'
 import Restaurante from './Restaurante'
 
+interface IParametrosBusca {
+  ordering?: string
+  search?: string
+}
+
 const ListaRestaurantes = () => {
 
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
   const [proximaPagina, setProximaPagina] = useState('')
   const [paginaAnterior, setPaginaAnterior] = useState('')
+  const [busca, setBusca] = useState('')
 
   useEffect(() => {
-    carregarDados('http://localhost:8000/api/v1/restaurantes/')
-  }, [])
+    const opcoes = {
+      params: {
 
-  const carregarDados = (url: string) => {
-    axios.get<IPaginacao<IRestaurante>>(url)
+      } as IParametrosBusca
+    }
+    if (busca) {
+      opcoes.params.search = busca
+    }
+
+    carregarDados('http://localhost:8000/api/v1/restaurantes/', opcoes)
+  }, [busca])
+
+  const carregarDados = (url: string, opcoes: AxiosRequestConfig = {}) => {
+    axios.get<IPaginacao<IRestaurante>>(url, opcoes)
       .then(resposta => {
         setRestaurantes(resposta.data.results)
         setProximaPagina(resposta.data.next)
@@ -29,6 +45,12 @@ const ListaRestaurantes = () => {
 
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+    <TextField
+      value={busca}
+      onChange={evento => setBusca(evento.target.value)}
+      label="Buscar restaurante"
+      variant="standard"
+    />
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
     {<button onClick={() => carregarDados(paginaAnterior)} disabled={!paginaAnterior}>
       Página Anterior
